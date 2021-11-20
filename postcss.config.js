e78cbb902e2d34928b50f944af2a8561c0c5a097
Journey Cruz <journey.cruz@gmail.com>
2021-11-19 20:17:00 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -10,12 +10,12 @@ module.exports = {
                 './pages/**/*.{js,jsx,ts,tsx}',
                 './components/**/*.{js,jsx,ts,tsx}',
               ],
-              defaultExtractor: (content) =>
-                content.match(/[\w-/:]+(?<!:)/g) || [],
+              // defaultExtractor: (content) => 
+              //   content.match(/[\w-/:]+(?<!:)/g) || [],
             },
           ],
         ]
       : []),
-    // 'postcss-preset-env',
+    'postcss-preset-env',
   ],
 }
