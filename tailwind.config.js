f5fb86b96e8ef5f3f29c3e0872a3b1fe8af10a0a
Journey Cruz <journey.cruz@gmail.com>
2021-11-19 21:41:52 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,15 +29,6 @@ module.exports = {
         success: "#0070f3",
         cyan: "#79FFE1",
       },
-      spacing: {
-        28: "7rem",
-      },
-      letterSpacing: {
-        tighter: "-.04em",
-      },
-      lineHeight: {
-        tight: 1.2,
-      },
       fontSize: {
         "5xl": "2.5rem",
         "6xl": "2.75rem",
