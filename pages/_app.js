35de62cbfbaef09b8ec38b47f6c31d8e8193c7ec
Journey Cruz <journey.cruz@gmail.com>
2021-11-22 09:55:57 -0600

    fb messenger plugin

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import Footer from "../components/footer";
 import "../styles/index.css";
 import "../styles/typography.css";
-import { AnimateSharedLayout } from "framer-motion"
+import { AnimateSharedLayout } from "framer-motion";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AnimateSharedLayout>
+      <MessengerCustomerChat pageId='110322431433744' appId='487501769175392' />
       <Component {...pageProps} />
       <Footer />
     </AnimateSharedLayout>
